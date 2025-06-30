007060d27472ab4a95c0dd2aa8892d8cb18de81f
Dustin <99244375+Dustin-F@users.noreply.github.com>
2025-06-30 17:09:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.get('/', (req, res) => res.send('API Running'));
 //define routes
 
 app.use('/api/users', require('./routes/api/users'));
-
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 
 
