3c7e072ced3159a636c46d06fa836bcc76a9e30f
Dusty <99244375+Dustin-F@users.noreply.github.com>
2025-07-24 22:42:52 +0800

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,93 +1,84 @@
 const express = require('express');
 const router = express.Router();
-const gravatar = require ('gravatar')
+const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const jwt = require ('jsonwebtoken');
-const config = require ('config');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
-
 //@route    POST api/users
 //@desc     Register user
 //@access   Public
+const validation = [
+  check('name', 'Name is required').not().isEmpty(),
+  check('email', 'Please include a valid email').isEmail(),
+  check(
+    'password',
+    'Please enter a password with 6 or more characters'
+  ).isLength({ min: 6 }),
+];
 
-router.post('/',
-    
-    [
-    check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6})
-] ,
+const handleCreateRequest = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
 
-async (req,res ) => 
-    {
+  const { name, email, password } = req.body;
 
-        const errors = validationResult(req);
-            if(!errors.isEmpty()) {
-                return res.status(400).json({errors: errors.array()});
-            }
+  try {
+    let user = await User.findOne({ email });
 
-       
-
-
-const {name, email, password} = req.body;
-
-try {
-    let user = await User.findOne({email})
-
-    if (user ) {
-      return  res.status(400).json({errors:[{msg: 'User already exists '}] });
+    if (user) {
+      return res
+        .status(400)
+        .json({ errors: [{ msg: 'User already exists ' }] });
     }
     //get users gravatar
-const avatar = gravatar.url(email,{
-s:'200',
-r: 'pg',
-d: 'mm'
-})
+    const avatar = gravatar.url(email, {
+      s: '200',
+      r: 'pg',
+      d: 'mm',
+    });
 
-user = new User({
-name,
-email,
-avatar,
-password
-});
-//encrypt password
+    user = new User({
+      name,
+      email,
+      avatar,
+      password,
+    });
+    //encrypt password
 
-const salt = await bcrypt.genSalt(10);
+    const salt = await bcrypt.genSalt(10);
 
-user.password = await bcrypt.hash(password, salt);
+    user.password = await bcrypt.hash(password, salt);
 
-await user.save();
+    await user.save();
 
+    // return json token
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
 
-
-// return json token
-const payload = {
-user:{
-    id: user.id
-}
-
-
-}
-
-jwt.sign(
-payload,
-config.get('jwtSecret'),
-{expiresIn: 360000},
-(err, token)=> {
-if(err) throw err;
-res.json({token});
- 
-})
-
-} catch(err){
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
-}
-    }
-);
+  }
+};
 
-
+router.post('/create', validation, handleCreateRequest);
+router.get('/getAll', handleRequest);
 module.exports = router;
